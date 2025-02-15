7e740e0c786b627ca0ebb2a733d9c08862c38df8
Prerna-Code04 <prerna44k@gmail.com>
2025-02-15 08:53:46 +0000

    conversions

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,31 @@ let state=null //object? aaya h
 let name= "people"
 
 console.log(typeof state);
+
+//conversion
+let score= "33abc" //value will store NaN wont throw any error
+let anotherScore= null //when null is converted, its value is 0
+console.log(typeof anotherScore);
+
+let valueInNum= Number(anotherScore)
+console.log(typeof valueInNum)
+console.log(valueInNum);
+//similarly for undefined we will have NaN, and if boolean value like true then 1 and if false then 0
+
+//boolean
+let isLoggedIn= " "
+let boolValue= Boolean(isLoggedIn)
+console.log(boolValue);
+
+//string
+let someVal= 123
+let stringValue= String(someVal)
+// let idk= Undefined( someVal) nothing like this exist
+// console.log(idk);
+
+console.log(stringValue);
+console.log(typeof stringValue);
+
+
+
+
